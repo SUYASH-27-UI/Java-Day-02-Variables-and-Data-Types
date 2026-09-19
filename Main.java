ded6d504a6b8daa101446746e8532b4ff4917ae6
SUYASH-27-UI <suyashsurve30@gmail.com>
2026-09-19 12:19:02 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String name = "Suyash";
+        int age = 21;
+        double marks = 78.5;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+}
